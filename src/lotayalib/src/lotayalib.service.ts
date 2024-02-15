f0968b560317b6c36bc6fcaccb0b92f9d622a57c
zinko2000htwe <112312429+zinko2000htwe@users.noreply.github.com>
2024-02-15 20:54:45 +0630

    202402150854

--- src/lotayalib/src/lotayalib.service.ts
+++ src/lotayalib/src/lotayalib.service.ts
@@ -1,5 +1,5 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../../prisma/lotaya_wallet';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class LotayaLibService extends PrismaClient implements OnModuleInit {
